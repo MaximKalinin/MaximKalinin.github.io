7da34a072c1e072e67f00b77f2f7bbb9708cc773
Maksim Kalinin <maxim.kalinin.work@gmail.com>
2019-08-04 18:50:54 +0500

    New website

---
+++ precache-manifest.5247618058c24d7ccabcea15bc3389b1.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0577f559c71c581d1e8720f5f7f5ca4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58a74f0ea6ea162cb0c5",
+    "url": "/static/css/main.a7370a00.chunk.css"
+  },
+  {
+    "revision": "ac6a50c87485ef147358",
+    "url": "/static/js/2.7e39c2eb.chunk.js"
+  },
+  {
+    "revision": "58a74f0ea6ea162cb0c5",
+    "url": "/static/js/main.9c4c598e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0b79c36a451c21020221f56f2089e49c",
+    "url": "/static/media/announce-preview-example.0b79c36a.png"
+  },
+  {
+    "revision": "56da5ef7b50a6f94c2cd0a96bca88e55",
+    "url": "/static/media/dortable-1.56da5ef7.png"
+  },
+  {
+    "revision": "243e9390d298f6df6e277c3ca528cba8",
+    "url": "/static/media/dortable-10.243e9390.png"
+  },
+  {
+    "revision": "5957e744406deb8ec414327850f74b95",
+    "url": "/static/media/dortable-11.5957e744.png"
+  },
+  {
+    "revision": "d14b9c3ef0818bfec1d0927ca99822e5",
+    "url": "/static/media/dortable-2.d14b9c3e.png"
+  },
+  {
+    "revision": "cd6de84abdaecf9a38122903ab8e3f2f",
+    "url": "/static/media/dortable-3.cd6de84a.png"
+  },
+  {
+    "revision": "da94420db79485559eb47879f515c3bc",
+    "url": "/static/media/dortable-4.da94420d.png"
+  },
+  {
+    "revision": "4486484f351d5d9ca3796c64518b5b45",
+    "url": "/static/media/dortable-5.4486484f.png"
+  },
+  {
+    "revision": "e8fb00b716553edafe3a572861d84e0e",
+    "url": "/static/media/dortable-6.e8fb00b7.png"
+  },
+  {
+    "revision": "2420451e5c93ff287fd30b9f2a929590",
+    "url": "/static/media/dortable-7.2420451e.png"
+  },
+  {
+    "revision": "0f0c3916864e315d122c294dfb6057fc",
+    "url": "/static/media/dortable-8.0f0c3916.png"
+  },
+  {
+    "revision": "f1ccaf5ec294075cff6e80a9d9c33939",
+    "url": "/static/media/dortable-9.f1ccaf5e.png"
+  },
+  {
+    "revision": "311b7fc943f6469bbe77fe30e939cfc2",
+    "url": "/static/media/heart.311b7fc9.svg"
+  }
+]);
